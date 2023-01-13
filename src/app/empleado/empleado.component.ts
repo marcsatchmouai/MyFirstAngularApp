6f74fc41f7042fafe4465cb718c57a2e686951a0
Marc Satchmo <marcsatchmo@gmail.com>
2023-01-13 00:41:38 -0300

    Videos 9, 10 y 11

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,7 +9,17 @@ export class EmpleadoComponent {
   nombre="Juan";
   apellido="Díaz";
   private edad=18;
-  //empresa="Pildoras informaticas";
+  empresa="Google";
+
+  habilitacionCuadro = false;
+
+  usuRegistrado = true;
+
+  textoDeRegistro = "No hay nadie registrado";
+
+  // cambiaEmpresa(event:Event) {
+  //   this.empresa = (<HTMLInputElement>event.target).value;
+  // }
 
   getEdad() {
     return this.edad;
@@ -17,6 +27,19 @@ export class EmpleadoComponent {
 
   getEmpresa(value:String) {
 
+  }
+  
+  getRegistroUsuario(){
+    this.usuRegistrado = false;
+  }
+
+  setUsuarioRegistrado(event:Event){
+    //alert("El usuario se acaba de registrar");
+    if((<HTMLInputElement>event.target).value=="si") {
+      this.textoDeRegistro = "El usuario se acaba de registrar"
+    } else {
+      this.textoDeRegistro = "No hay nadie registrado"
+    }
   }
 
   constructor() {}
